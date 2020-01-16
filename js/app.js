bc4ee56846248c680e57175e560f0448c0c5fd54
Lesley Rivera <les.aguilar@live.com>
2020-01-15 21:18:07 -0800

    about me

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+alert('Wizards Welcome (Muggles Tolerated!)');
+
+console.log('Welcome');
+
+var welcomeMessage = prompt('Are you ready to guess who I am?');
+
+var aboutMe = prompt('Am I a female?')
+
+prompt('Am I tall?')
+
+prompt('')
+
+
+
+
+
+
