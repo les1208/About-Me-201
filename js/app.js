99f75214ef2d9314e3b355295c57609b2cf67bc6
Lesley Rivera <les.aguilar@live.com>
2020-01-16 19:30:28 -0800

    recent about me

--- js/app.js
+++ js/app.js
@@ -2,15 +2,45 @@
 
 alert('Wizards Welcome (Muggles Tolerated!)');
 
-console.log('Welcome');
-
 var welcomeMessage = prompt('Are you ready to guess who I am?');
+if (welcomeMessage.toLowerCase() === 'yes') {
+    alert('Great, lets get started!');
+} else if (welcomeMessage.toLowerCase() === 'no') {
+    alert('Error');
+}
 
-var aboutMe = prompt('Am I a female?')
+var aboutMe = prompt('Am I a female?');
+if (aboutMe.toLowerCase() === 'yes') {
+    alert('You are correct, five points for Slytherin!');
+} else if (aboutMe.toLowerCase() === 'no') {
+    alert('Try again');
+}
 
-prompt('Am I tall?')
 
-prompt('')
+var myHeight = prompt('Am I tall?');
+if (myHeight.toLowerCase() === 'yes') {
+    alert('Another five points for Slytherin');
+} else if (myHeight.toLowerCase() === 'no') {
+    alert('you lose 10 points')
+}
+
+var whatLanguage = prompt('Do I speak another language?');
+if (whatLanguage.toLowerCase() === 'yes') {
+    alert('More points for Slytherin');
+} else if (whatLanguage.toLowerCase() === 'no') {
+    alert('You are on a role losing points!')
+}
+
+var whatDoI  = prompt('Is spanish the language I speak?');
+if (whatDoI.toLowerCase() === 'yes') {
+    alert('Ding, ding, ding!');
+} else if (whatLanguage1.toLowerCase() === 'no') {
+    alert('Try again!');
+}
+
+
+
+
 
 
 
