2a062bc5079e616c621713d2fc69c1a664967c29
Lesley Rivera <les.aguilar@live.com>
2020-01-18 17:19:14 -0800

    lab 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 alert('Wizards Welcome (Muggles Tolerated!)');
+console.log('I am in app.js');
 
 var welcomeMessage = prompt('Are you ready to guess who I am?');
 if (welcomeMessage.toLowerCase() === 'yes') {
@@ -15,7 +16,6 @@ if (aboutMe.toLowerCase() === 'yes') {
 } else if (aboutMe.toLowerCase() === 'no') {
     alert('Try again');
 }
-
 
 var myHeight = prompt('Am I tall?');
 if (myHeight.toLowerCase() === 'yes') {
@@ -38,12 +38,34 @@ if (whatDoI.toLowerCase() === 'yes') {
     alert('Try again!');
 }
 
+// guessing game
 
+var guessNumber = prompt('Guess the correct number. 1-10');
 
+var answer= 6;
 
+for (var i=0; i<4; i++) {
+    var guess = parseInt(prompt('Whats your guess?'));
+     if(guess === answer) {
+        alert('Your answer was correct');
+        break;
+     } else if(guess > answer) {
+        alert('Sorry too high'); 
+     } else if(guess < answer) {
+        alert('Sorry too low');
+     }
+}
 
+// 7th question
 
+var guessedHouse = prompt('Guess the houses that are a part of Hogwarts?');
 
+var hogwartsHouses = ['Gryffindor', 'Hufflepuff', 'Slytherin'];
+console.log('Full Array: ' + hogwartsHouses);
 
-
-
+if(guessedHouse === hogwartsHouses[0].toLowerCase() || hogwartsHouses[1].toLowerCase() || hogwartsHouses[2].toLowerCase()) {
+    alert('Your answer is correct, all the Hogwarts houses are Gryffindor, Slytherin, and Hufflepuff');    
+} else {
+    alert('You guessed incorrectly, no all the Hogwarts houses are Gryffindor, Slytherin, and Hufflepuff');
+    
+}
