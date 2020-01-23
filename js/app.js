96e97a51ea9255980747cc80eceb5b2bbe1f0c24
Lesley Rivera <les.aguilar@live.com>
2020-01-22 18:51:02 -0800

    fixed bug

--- js/app.js
+++ js/app.js
@@ -1,41 +1,59 @@
 'use strict';
 
-alert('Wizards Welcome (Muggles Tolerated!)');
-console.log('I am in app.js');
+var correctAnswer = 0;
 
-var welcomeMessage = prompt('Are you ready to guess who I am?');
-if (welcomeMessage.toLowerCase() === 'yes') {
-    alert('Great, lets get started!');
-} else if (welcomeMessage.toLowerCase() === 'no') {
-    alert('Error');
-}
 
-var aboutMe = prompt('Am I a female?');
-if (aboutMe.toLowerCase() === 'yes') {
-    alert('You are correct, five points for Slytherin!');
-} else if (aboutMe.toLowerCase() === 'no') {
-    alert('Try again');
-}
+alert('Wizards Welcome (Muggles Tolerated!) What is your name?');
+console.log('User typed: ' + username);
 
-var myHeight = prompt('Am I tall?');
-if (myHeight.toLowerCase() === 'yes') {
+function questionOne() {
+    var game = prompt('Are you ready to guess who I am?')
+
+    if(questionOne.toLowerCase() === 'yes') {
+        alert('Great, lets get started');
+    } else if(questionOne.toLowerCase() === 'no') {
+        alert('Error');
+    }
+} questionOne();
+
+function questionTwo() {
+    var identity = prompt('Am I a female?');
+
+    if (questionTwo.toLowerCase() === 'yes') {
+        alert('You are correct, five points for Slytherin!');
+    } else if(questionTwo.toLowerCase() === 'no') {
+        alert('Try again');
+    }
+} questionTwo();
+
+function questionThree() {
+    var myHeight = prompt('Am I tall?');
+if (questionThree.toLowerCase() === 'yes') {
     alert('Another five points for Slytherin');
-} else if (myHeight.toLowerCase() === 'no') {
+    correctAnswer++;
+} else if (questionThree.toLowerCase() === 'no') {
     alert('you lose 10 points')
+
 }
+} questionThree();
+
 
 var whatLanguage = prompt('Do I speak another language?');
 if (whatLanguage.toLowerCase() === 'yes') {
     alert('More points for Slytherin');
+    correctAnswer++;
 } else if (whatLanguage.toLowerCase() === 'no') {
     alert('You are on a role losing points!')
+    
 }
 
 var whatDoI  = prompt('Is spanish the language I speak?');
 if (whatDoI.toLowerCase() === 'yes') {
     alert('Ding, ding, ding!');
-} else if (whatLanguage1.toLowerCase() === 'no') {
+    correctAnswer++;
+} else if (whatLanguage.toLowerCase() === 'no') {
     alert('Try again!');
+    
 }
 
 // guessing game
@@ -48,6 +66,7 @@ for (var i=0; i<4; i++) {
     var guess = parseInt(prompt('Whats your guess?'));
      if(guess === answer) {
         alert('Your answer was correct');
+        correctAnswer++;
         break;
      } else if(guess > answer) {
         alert('Sorry too high'); 
@@ -64,8 +83,10 @@ var hogwartsHouses = ['Gryffindor', 'Hufflepuff', 'Slytherin'];
 console.log('Full Array: ' + hogwartsHouses);
 
 if(guessedHouse === hogwartsHouses[0].toLowerCase() || hogwartsHouses[1].toLowerCase() || hogwartsHouses[2].toLowerCase()) {
-    alert('Your answer is correct, all the Hogwarts houses are Gryffindor, Slytherin, and Hufflepuff');    
+    alert('Your answer is correct, all the Hogwarts houses are Gryffindor, Slytherin, and Hufflepuff');  
+    correctAnswer++; 
 } else {
     alert('You guessed incorrectly, no all the Hogwarts houses are Gryffindor, Slytherin, and Hufflepuff');
-    
+   
+    correctAnswer + ' out of ' + questions.length;
 }
